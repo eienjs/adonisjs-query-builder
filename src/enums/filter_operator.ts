fed456a451ac0a23fc94f6ab32c85d47b5e44172
Fernando IL <luffynando@gmail.com>
2024-11-13 16:58:59 -0600

    feat: :sparkles: add enums

---
+++ src/enums/filter_operator.ts
@@ -0,0 +1,9 @@
+export const FilterOperator = {
+  Dynamic: '',
+  Equal: '=',
+  LessThan: '<',
+  GreaterThan: '>',
+  LessThanOrEqual: '<=',
+  GreaterThanOrEqual: '>=',
+  NotEqual: '<>',
+} as const;
