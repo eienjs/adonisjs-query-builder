ea2a9374f38392280a60b6910944661f5be4cc9c
Fernando IL <luffynando@gmail.com>
2024-11-14 11:33:30 -0600

    feat: :sparkles: add filters operator

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
 import { type LucidModel, type ModelQueryBuilderContract } from '@adonisjs/lucid/types/model';
 import { type StrictValuesWithoutRaw } from '@adonisjs/lucid/types/querybuilder';
 
+export type EnumValues<T> = T[keyof T];
+
 export interface Filter<Model extends LucidModel, Result = InstanceType<Model>> {
   _invoke(query: ModelQueryBuilderContract<Model, Result>, value: StrictValuesWithoutRaw, property: string): void;
 }
