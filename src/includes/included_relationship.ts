b0a390464cda385a6928acc88659d2b1a9a98cd2
Fernando Isidro Luna <luffynando@gmail.com>
2024-11-18 23:57:53 -0600

    chore: :construction: handle relationships using recursive preload

--- src/includes/included_relationship.ts
+++ src/includes/included_relationship.ts
@@ -1,15 +1,10 @@
 import { type LucidModel, type ModelQueryBuilderContract } from '@adonisjs/lucid/types/model';
+import { type ExtractModelRelations } from '@adonisjs/lucid/types/relations';
 import { type Include } from '../types.js';
 
 export default class IncludedRelationship<Model extends LucidModel, Result = InstanceType<Model>>
   implements Include<Model, Result>
 {
-  public getRequestedFieldsForRelatedTable?: (relationship: string) => string[];
-
-  public _invoke(query: ModelQueryBuilderContract<Model, Result>, include: string): void {
-    const relatedTables = include.split('.');
-  }
-
   public static getIndividualRelationshipPathsFromInclude(include: string): string[] {
     const includes: string[] = [];
 
@@ -24,5 +19,40 @@ export default class IncludedRelationship<Model extends LucidModel, Result = Ins
     }
 
     return includes;
+  }
+
+  public getRequestedFieldsForRelatedTable?: (relationship: string) => string[];
+
+  public _invoke(query: ModelQueryBuilderContract<Model, Result>, include: string): void {
+    const relatedTables = include.split('.');
+    this.buildNestedQuery(relatedTables, query);
+  }
+
+  private buildNestedQuery<SubModel extends LucidModel, IntanceResult = InstanceType<SubModel>>(
+    relatedTables: string[],
+    subQuery: ModelQueryBuilderContract<SubModel, IntanceResult>,
+    parent = '',
+  ): void {
+    if (relatedTables.length === 0) {
+      return;
+    }
+
+    const [relation, ...rest] = relatedTables;
+
+    if (!subQuery.model.$hasRelation(relation)) {
+      return;
+    }
+
+    void subQuery.preload(relation as ExtractModelRelations<InstanceType<SubModel>>, (nestedQuery) => {
+      const fullRelationName = parent === '' ? relation : `${parent}.${relation}`;
+      if (this.getRequestedFieldsForRelatedTable !== undefined) {
+        const fields = this.getRequestedFieldsForRelatedTable(fullRelationName);
+        if (fields.length > 0) {
+          void nestedQuery.select(fields);
+        }
+      }
+
+      this.buildNestedQuery(rest, nestedQuery, fullRelationName);
+    });
   }
 }
